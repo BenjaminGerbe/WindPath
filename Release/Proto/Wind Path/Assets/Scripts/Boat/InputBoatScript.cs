95b343c75d8a387d008b5ac02202e3b21b07e4ec
PinoPaprika <benjamin.gerbe@gmail.com>
2021-12-06 18:58:58 +0100

    bateau  mouvement animation

--- Release/Proto/Wind Path/Assets/Scripts/Boat/InputBoatScript.cs
+++ Release/Proto/Wind Path/Assets/Scripts/Boat/InputBoatScript.cs
@@ -2,23 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputBoatScript : MonoBehaviour
+public  class InputBoatScript : MonoBehaviour
 {
     /// <summary>
     /// Script fait par : Benjamin
     /// Utilisé pour : Génrer les input
     /// </summary>
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    private bool RT = false;
+    public virtual bool isTurningRight()
     {
-        
+        if (Input.GetAxisRaw("Horizontal") > 0 && !RT)  return  true;
+        return false;
     }
+    
+    public virtual bool isTurningLeft()
+    {
+   
+        if (Input.GetAxisRaw("Horizontal") < 0 && !RT )  return  true;
+        
+     
+        return false;
+    }
+    
+    public virtual bool isAccelerate()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0 && !RT)  return  true;
+        return false;
+    }
+    
+    public virtual bool sailIsRight()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0 )  return  true;
+        return false;
+    }
+    
+    public virtual bool sailIsLeft()
+    {
+   
+        if (Input.GetAxisRaw("Horizontal") < 0 )  return  true;
+        
+     
+        return false;
+    }
+    
+    
+
+    public virtual bool isRT()
+    {
+        if (Input.GetAxisRaw("Sail") > 0)
+        {
+            RT = true;
+            return true;
+        }
+
+        RT = false;
+        return false;
+    }
+    
+
 }
