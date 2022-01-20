ad6d6f41af9fc5a5dda2720380ea42a551586810
Reshybraz <59066353+Reshybraz@users.noreply.github.com>
2022-01-20 13:31:09 +0100

    ajout temps de course + ajout GUI

--- Release/Proto/Wind Path/Assets/Scripts/CountTour.cs
+++ Release/Proto/Wind Path/Assets/Scripts/CountTour.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CountTour : MonoBehaviour
 {
+    /// <summary>
+    /// Script fait par: Julien
+    /// Utilisé pour : Passage des tours de circuit et temps de parcours global (UI intégré)
+    /// </summary>
 
+    private float RaceTime;
+    private string RealRaceTime;
+    private float StartTime = 0;
     private int ActualTour = 0;
     private bool check = true;
     public int nbTour;
 
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (StartTime != 0 && ActualTour <= nbTour)
+        {
+            RaceTime = Time.time - StartTime;
+            TimeSpan t = TimeSpan.FromSeconds(RaceTime);
+            RealRaceTime = t.ToString(@"mm\:ss\:fff");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,9 +37,13 @@ public class CountTour : MonoBehaviour
             check = false;
             ActualTour++;
             Debug.Log(ActualTour);
+            if (ActualTour == 1)
+            {
+                StartTime = Time.time;
+            } 
             if (ActualTour > nbTour)
             {
-
+                
             }
         }
         if (other.name=="checkpoint")
@@ -35,6 +54,15 @@ public class CountTour : MonoBehaviour
 
     void OnGUI()
     {
-        GUI.Label(new Rect(300, 100, 400, 80), (ActualTour.ToString()));
+        string tour = "";
+        if (ActualTour > nbTour)
+        {
+            tour = "finish";
+        }
+        else
+        {
+            tour = ActualTour.ToString();
+        }
+        GUI.Label(new Rect(300, 100, 400, 80), tour+"\n"+RealRaceTime);
     }
 }
