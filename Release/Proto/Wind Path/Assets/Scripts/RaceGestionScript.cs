d6558332aa9a78003a29c9f4cf348039782adca8
Reshybraz <59066353+Reshybraz@users.noreply.github.com>
2022-03-19 14:00:40 +0100

    Update RaceGestionScript.cs

--- Release/Proto/Wind Path/Assets/Scripts/RaceGestionScript.cs
+++ Release/Proto/Wind Path/Assets/Scripts/RaceGestionScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class RaceGestionScript : MonoBehaviour
 {
-
+    /// <summary>
+    /// Script fait par: Julien
+    /// Utilisé pour : tours de tous les joueurs + classement
+    /// </summary>
     public int nbTour;
     public Collider[] Checkpoint;
     // Start is called before the first frame update
